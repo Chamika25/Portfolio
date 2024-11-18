9020e3aee0477543f00d03af8c583ff52ac1f120
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-18 11:43:01 +0530

    Social medias data

---
+++ data/social.js
@@ -0,0 +1,22 @@
+import { FaGithub, FaLinkedin, FaYoutube, FaTwitter } from "react-icons/fa";
+
+const social = [
+    {
+        icon: <FaGithub />,
+        url: "https://github.com/your-username"
+    },
+    {
+        icon: <FaLinkedin />,
+        url: "https://www.linkedin.com/in/your-username"
+    },
+    {
+        icon: <FaYoutube />,
+        url: "https://www.youtube.com/channel/your-channel-id"
+    },
+    {
+        icon: <FaTwitter />,
+        url: "https://twitter.com/your-username"
+    },
+];
+
+export default social;
