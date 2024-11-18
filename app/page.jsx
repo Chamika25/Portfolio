7c0401166157f4597c74dfbe4978bb32a0ab4970
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-18 11:43:41 +0530

    Home page edit

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,41 @@
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+// Components
+import Social from "@/components/Social";
+
 export default function Home() {
   return (
-    <main>Home page</main>
+    <section>
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* Text */}
+          <div className="text-center xl:text-left">
+            <h1 className="h1 mb-6">
+              Hello, I'm <br/> <span className="text-accent">Buddika Kasun</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experiences and I am proficient in verious programming languages and thechnology.
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              {/* CV */}
+              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
+                <span>Download CV</span>
+                <FiDownload className="text-xl" />
+              </Button>
+              {/* Social media */}
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyle="flex gap-6"
+                  iconStyle="w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"
+                />
+              </div>
+            </div>
+          </div>
+          {/* Photo */}
+          <div>photo</div>
+        </div>
+      </div>
+    </section>
   );
 }
