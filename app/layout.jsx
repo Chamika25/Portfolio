775c0bb8ed84522a5bd0ff038b39643b56a4fbba
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-19 12:39:10 +0530

    Add particales bg & custom cursor

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import Header from '@/components/Header';
 import PageTransition from '@/components/pageTransition';
 import StairTransition from '@/components/StairTransition';
+import ParticlesBackground from '@/components/ParticlesBackground';
+import CustomCursor from '@/components/CustomCursor';
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -21,6 +23,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={jetbrainsMono.variable} >
+        <ParticlesBackground />
+        <CustomCursor />
         <Header />
         <StairTransition />
         <PageTransition>
