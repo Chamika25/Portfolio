6a7e74a7805b7fa69793b25f8397789052b1562e
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-20 11:32:54 +0530

    Add motion to social buttons

--- components/Social.jsx
+++ components/Social.jsx
@@ -2,19 +2,22 @@ import Link from "next/link";
 
 // Social items
 import social from "@/data/social";
+import MotionBtn from "./MotionBtn";
 
 const Social = ({ containerStyle, iconStyle}) => {
-  return (
-    <div className={containerStyle}>
-        {social.map((item, index) => {
-            return (
-                <Link href={item.url} key={index} className={iconStyle}>
-                    {item.icon}
-                </Link>
-            );
-        })}
-    </div>
-  )
+    return (
+        <div className={containerStyle}>
+            {social.map((item, index) => {
+                return (
+                    <MotionBtn key={index} whileHover={{ rotate:360, scale:1.5 }} transition={{ duration:0.5, type: "spring", stiffness: 400, damping: 10, }}>
+                        <Link href={item.url} key={index} className={iconStyle}>
+                            {item.icon}
+                        </Link>
+                    </MotionBtn>
+                );
+            })}
+        </div>
+    );
 }
 
-export default Social
+export default Social;
