79605dd1f9a7909fc094ddaa3fa778bbf4634595
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-20 16:45:53 +0530

    Stair fix & update

--- components/Stair.jsx
+++ components/Stair.jsx
@@ -1,15 +1,17 @@
-import { motion } from "framer-motion";
+"use client"
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
 
 // Variants
 const stairAnimation = {
     initial: {
-        top: "0%",
-    },
-    animate: {
         top: "100%",
     },
-    exit: {
+    animate: {
         top: ["100%", "0%"],
+    },
+    exit: {
+        top: ["0%", "100%"],
     },
 }
 
@@ -19,27 +21,44 @@ const reverseIndex = (index) => {
 }
 
 const Stair = () => {
+
+    const [isVisible, setIsVisible] = useState(true); // State to toggle visibility
+
+  useEffect(() => {
+    // Auto exit after 3 seconds
+    const timer = setTimeout(() => {
+      setIsVisible(false); // Trigger exit animation by setting visibility to false
+    }, 900); // Change the delay (in ms) as needed
+
+    // Cleanup the timer when component is unmounted
+    return () => clearTimeout(timer);
+  }, []);
+
     return (
-        <>
-            {/* Render 6 motion divs */}
-            {[...Array(8)].map((_, index) => {
-                return(
-                    <motion.div
-                        key={index}
-                        variants={stairAnimation}
-                        initial='initial'
-                        animate='animate'
-                        exit='exit'
-                        transition={{
-                            duration: 0.2,
-                            ease: "easeInOut",
-                            delay: reverseIndex(index) * 0.1,
-                        }}
-                        className="h-full w-full bg-accent-hover relative border border-primary"
-                    />
-                );
-            })}
-        </>
+        <AnimatePresence>
+            {isVisible && (
+                <>
+                    {[...Array(8)].map((_, index) => {
+                        return(
+                            <motion.div
+                                key={index}
+                                variants={stairAnimation}
+                                initial='initial'
+                                animate='animate'
+                                exit='exit'
+                                transition={{
+                                    duration: 0.2,
+                                    ease: "easeInOut",
+                                    //delay: reverseIndex(index) * 0.1, // right to left
+                                    delay: index * 0.1, // left to right
+                                }}
+                                className="h-full w-full bg-accent-hover relative border border-primary"
+                            />
+                        );
+                    })}
+                </>
+            )}
+        </AnimatePresence>
     );
 }
 
