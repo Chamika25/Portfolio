39febc7a1fd692cd357a8c28b477533484fb5358
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-20 16:45:32 +0530

    Stair transition comp edit

--- components/StairTransition.jsx
+++ components/StairTransition.jsx
@@ -1,32 +1,32 @@
 "use client"
 
 import { AnimatePresence, delay, motion } from 'framer-motion';
-import { usePathname } from 'next/navigation';
+//import { usePathname } from 'next/navigation';
 
 // Components
 import Stair from './Stair';
 
 const StairTransition = () => {
 
-    const pathName = usePathname();
+    //const pathName = usePathname();
 
     return (
         <>
             <AnimatePresence mode='wait'>
-                <div key={pathName}>
+                <div>
                     <div className='h-screen w-screen fixed left-0 right-0 pointer-events-none z-40 flex top-0 lg:top-20' /* top-0 */>
                         <Stair />
                     </div>
 
-                    <motion.div
-                        className='h-screen w-screen fixed bg-primary pointer-events-none top-0 lg:top-20' /* top-0 */
+                    {/*<motion.div
+                        className='h-screen w-screen fixed bg-primary pointer-events-none top-0 lg:top-20'
                         initial={{opacity: 1}}
                         animate={{opacity: 0, transition: {
                             delay: 0,
                             duration: 0.4,
                             ease: 'easeInOut',
                         }}}
-                    />
+                    /> */}
 
                 </div>
             </AnimatePresence>
