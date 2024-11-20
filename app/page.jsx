cf04679c192511edfc4d63e667f871a178d1f9a8
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-20 16:50:10 +0530

    Main single page

--- app/page.jsx
+++ app/page.jsx
@@ -1,46 +1,80 @@
-import { Button } from "@/components/ui/button";
-import { FiDownload } from "react-icons/fi";
+// // Pages
+// import Home from "./(pages)/Home";
+// import Stats from "@/components/Stats";
+
+// // Components
+// import CustomCursor from "@/components/CustomCursor";
+// import ParticlesBackground from "@/components/ParticlesBackground";
+// import Header from "@/components/Header";
+// import CustomScroll from "@/components/CustomScroll";
+// import PageTransition from "@/components/pageTransition";
+
+// export default function Page() {
+//     return (
+//         <>
+//             <ParticlesBackground />
+//             <CustomCursor />
+
+//             {/* Header */}
+//             <Header />
+
+//             {/* Body */}
+//             <CustomScroll>
+
+//               {/* Home */}
+//               <section id="home">
+//                 <PageTransition id="home">
+//                   <Home />
+//                 </PageTransition>
+//               </section>
+
+//               <section id="service">
+//                 <PageTransition id="service">
+//                   <Stats />
+//                 </PageTransition>
+//               </section>
+
+//             </CustomScroll>
+//         </>
+//     );
+// }
+
+
+//2
+
+//pages
+import Home from "./(pages)/Home";
+import Service from "./(pages)/Service";
 
 // Components
-import Social from "@/components/Social";
-import Photo from "@/components/Photo";
-import Stats from "@/components/Stats";
+import CustomCursor from "@/components/CustomCursor";
+import ParticlesBackground from "@/components/ParticlesBackground";
+import Header from "@/components/Header";
+import CustomScroll from "@/components/CustomScroll";
+import PageTransition from "@/components/pageTransition";
 
-export default function Home() {
+export default function Page() {
+
   return (
-    <section>
-      <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-8">
-          {/* Text */}
-          <div className="text-center xl:text-left order-2 xl:order-none">
-            <h1 className="h1 mb-6">
-              <span className="w">Hello, I'm</span><br/><span className="text-accent">Buddika Kasun</span>
-            </h1>
-            <p className="max-w-[500px] mb-9 text-white/80 w">
-              I excel at crafting elegant digital experiences and I am proficient in verious programming languages and thechnology.
-            </p>
-            <div className="flex flex-col xl:flex-row items-center gap-8">
-              {/* CV */}
-              <Button variant="outline" size="lg" className="uppercase flex items-center gap-2 btn">
-                <span>Download CV</span>
-                <FiDownload className="text-xl" />
-              </Button>
-              {/* Social media */}
-              <div className="mb-8 xl:mb-0">
-                <Social
-                  containerStyle="flex gap-6"
-                  iconStyle="w-9 h-9 border-[1.5px] border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500 cursor-none"
-                />
-              </div>
-            </div>
-          </div>
-          {/* Photo */}
-          <div className="order-1 xl:order-none mb-8 xl:mb-0 xl:mr-20">
-            <Photo />
-          </div>
-        </div>
-      </div>
-      <Stats />
-    </section>
+    <>
+      <ParticlesBackground />
+      <CustomCursor />
+
+      {/* Header */}
+      <Header />
+
+      {/* Body */}
+      <CustomScroll>
+
+        <section id="home">
+            <Home />
+        </section>
+
+        <section id="service">
+            <Service />
+        </section>
+
+      </CustomScroll>
+    </>
   );
 }
