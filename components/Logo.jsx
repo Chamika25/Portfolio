d570f1928d9d43f79743050b3efb29d15fd0f279
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-22 14:48:37 +0530

    Logo edit

--- components/Logo.jsx
+++ components/Logo.jsx
@@ -94,10 +94,11 @@ const Logo = () => {
   const letterVariants = {
     initial: { y: 0 }, // Default position
     hover: (i) => ({
-      y: [0, -10, 0], // Bounce animation
+      y: [0, -15, 0], // Bounce animation
       transition: {
         duration: 0.5,
         repeat: Infinity,
+        repeatDelay: 10,
         delay: i * 0.1, // Staggered effect for each letter
       },
     }),
@@ -105,9 +106,34 @@ const Logo = () => {
 
   return (
     <motion.div
+      initial={{
+        opacity: 0,
+        x: -200,
+      }}
+      animate={{
+        opacity: 1,
+        x: 0,
+      }}
+      transition= {{
+        opacity: {
+          delay: 1,
+          duration: 1.5,
+          ease: "easeInOut",
+        },
+        x: {
+          delay: 1,
+          duration: 2,
+          ease: "easeInOut",
+          type: "spring",
+          stiffness: 100,
+          damping: 8,
+        }
+      }}
+    >
+    <motion.div
       className="pl-4 flex space-x-1 text-4xl font-semibold"
       initial="initial"
-      whileHover="hover" // Trigger hover animation for all children
+      animate="hover" // Trigger hover animation for all children
     >
       {letters.map((char, index) => (
         <motion.span
@@ -125,10 +151,8 @@ const Logo = () => {
         .
       </motion.span>
     </motion.div>
+    </motion.div>
   );
 };
 
 export default Logo;
-
-
-
