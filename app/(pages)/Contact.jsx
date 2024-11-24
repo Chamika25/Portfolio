158c12ab109991e3028a3fc9f0205cdcbe6fbf61
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-24 18:56:13 +0530

    Remove not used important

--- app/(pages)/Contact.jsx
+++ app/(pages)/Contact.jsx
@@ -3,17 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue
-} from "@/components/ui/select";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
-import { delay, motion } from "framer-motion";
 
 const info = [
     {
