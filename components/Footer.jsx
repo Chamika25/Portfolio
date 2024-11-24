788bd615216babfd5b4f054c7703863788202dbb
Buddika Kasun <buddikakasun80@gmail.com>
2024-11-24 18:49:26 +0530

    Add footer component

---
+++ components/Footer.jsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+        <footer className="h-16 flex items-center justify-center bg-[#193432cc] shadow-lg">
+            Footer
+        </footer>
+    )
+}
+
+export default Footer;
