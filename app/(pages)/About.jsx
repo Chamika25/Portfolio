a835515c6eb77de5340c2c6fb585da6988a7b1bc
Buddika Kasun <buddikakasun80@gmail.com>
2024-12-05 12:00:05 +0530

    Add education sub section

--- app/(pages)/About.jsx
+++ app/(pages)/About.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import AboutNav from "@/components/AboutNav";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import EducationSubSection from "@/components/EducationSubSection";
 import { motion } from "framer-motion";
 
 const About = () => {
@@ -17,36 +17,27 @@ const About = () => {
                     ease: "easeIn"
                 }
             }}
-            className="min-h-[80px] flex items-center justify-center py-0"
         >
             <div className="container mx-auto relative">
-                {/* <Tabs
-                    defaultValue="experience"
-                    className="flex flex-col xl:flex-row gap-[60px]"
-                >
-                    <TabsList className="flex flex-col w-full max-w-[300px] mx-auto xl:mx-0 gap-6">
-                        <TabsTrigger value="experience">Experience</TabsTrigger>
-                        <TabsTrigger value="education">Education</TabsTrigger>
-                        <TabsTrigger value="skills">Skills</TabsTrigger>
-                        <TabsTrigger value="about">About me</TabsTrigger>
-                    </TabsList>
-
-                    <div className="min-h-[70vh] w-full">
-                        <TabsContent value="experience">Experience</TabsContent>
-                    </div>
-                </Tabs>*/}
                 <div className="flex flex-col xl:flex-row gap-5">
                     {/* Menu */}
-                    <div className="flex flex-col w-full max-w-80 h-fit mx-auto xl:mx-0 gap-6 sticky top-[100px] xl:top-0 xl:pt-[100px] border border-white z-10">
+                    <div className="flex flex-col w-full max-w-80 h-fit mx-auto xl:mx-0 gap-6 sticky top-[100px] xl:top-[20vh] xl:pt-[100px] z-10">
                         <AboutNav />
                     </div>
 
                     {/* Content */}
                     <div className="relative top-[-220px] xl:top-0 z-0">
+
                         <div className="aboutSubSection" id="experience">Experience</div>
-                        <div className="aboutSubSection" id="education">Education</div>
+
+                        <div className="aboutSubSection" id="education">
+                            <EducationSubSection />
+                        </div>
+
                         <div className="aboutSubSection" id="skills">Skills</div>
+
                         <div className="aboutSubSection" id="aboutMe">About Me</div>
+
                     </div>
                 </div>
             </div>                
