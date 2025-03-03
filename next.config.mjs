09cec62ea9b7ad5dea4532b0c11031ebf9af2fe1
Chamika1225 <chamikadilharaw@gmail.com>
2025-03-03 23:28:15 +0530

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,7 @@
 /** @type {import('next').NextConfig} */
-/*
 const nextConfig = {
   output: "export",
 };
 
-// next.config.js
-
-module.exports = {
-  target: 'serverless',
-};
-
-
-export default nextConfig;
- */
-
-// next.config.js
-const nextConfig = {
-  output: 'export', // For static export
-  target: 'serverless', // For serverless deployment on Netlify
-};
 
 export default nextConfig;
